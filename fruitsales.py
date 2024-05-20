60a3ec5fdd162f0b578a738d1ef29b5cd3b34476
ianpemb01 <ianpemb01@gmail.com>
2024-05-20 03:33:58 -0400

    Final commit

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,10 @@
 # add your code here
+import pandas as pd
+
+df_myvar=pd.DataFrame({'Apples':['35', '41'],
+              'Bananas':['21', '34']}, 
+              index=['2017 Sales', '2018 Sales'])
+
+df_myvar.to_csv("fruit.csv")
+
+print(df_myvar)
